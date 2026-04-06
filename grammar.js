38fee194b49fabd58ee6aaa7515a0577764b3ebd
Luis Vegas <luisvegasmor@gmail.com>
2026-04-06 11:36:47 -0400

    indentation

--- grammar.js
+++ grammar.js
@@ -2,14 +2,10 @@
 // @ts-check
 export default grammar({
   name: "maude",
-
   word: $ => $.identifier,
-
   extras: $ => [/\s/],
-
   rules: {
     source_file: $ => repeat($._token),
-
     _token: $ => choice(
       $.module_entry,
       $.keyword,
@@ -18,7 +14,6 @@ export default grammar({
       $.identifier,
       $.other
     ),
-
     module_entry: $ => seq(
       $.module_type,
       $.identifier,
@@ -26,16 +21,14 @@ export default grammar({
       repeat($._token),
       $.module_end
     ),
-
     keyword: $ => choice(
       "including", "pr", "protecting",
       "op", "sort", "sorts", "eq", "rl", "is", "var", "vars"
     ),
-
     op_attr: $ => choice("assoc", "comm"),
     module_type: $ => choice("fmod", "omod", "mod"),
     module_end: $ => choice("endfm", "endm", "endom"),
-      base_type: $ => choice("Nat", "NAT", "Bool", "BOOL", "FLOAT", "Float", "STRING", "String", "INT", "Int"),
+    base_type: $ => choice("Nat", "NAT", "Bool", "BOOL", "FLOAT", "Float", "STRING", "String", "INT", "Int"),
     identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
     other: $ => /[^\s]+/,
   }
